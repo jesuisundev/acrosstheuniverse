55f7f5a88bb5abf5b1fa2465b2fb82025cc5a3c5
jesuisundev <admin@jesuisundev.com>
2021-03-28 23:43:16 -0400

    basic starfield generator

--- public/libs/app_part_2.js
+++ public/libs/app_part_2.js
@@ -25,6 +25,42 @@ camera.position.y = 10
 camera.position.z = 0
 camera.lookAt(0, 0, 0)
 
+function getStarsGeometry() {
+    const geometry = new THREE.BufferGeometry()
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(getStarsRandomVertices(), 3))
+
+    return geometry
+}
+
+function getStarsRandomVertices(verticesNumber = 10000) {
+    const vertices = []
+
+    for (let i = 0; i < verticesNumber; i++) {
+        const x = 2000 * Math.random() - 1000;
+        const y = 2000 * Math.random() - 1000;
+        const z = 2000 * Math.random() - 1000;
+
+        vertices.push(x, y, z);
+    }
+
+    return vertices
+}
+
+function getStarsMaterial() {
+    const starSprite = new THREE.TextureLoader().load('../images/star.png');
+    const starMaterial = new THREE.PointsMaterial({ size: 5, sizeAttenuation: true, map: starSprite, alphaTest: 0.5, transparent: true });
+
+    return starMaterial
+}
+
+function getStars() {
+    const stars = new THREE.Points(getStarsGeometry(), getStarsMaterial())
+
+    return stars
+}
+
+scene.add(getStars())
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight)
