2bca6b6f33206b603b27bc4b19bb8daccc360c8e
jesuisundev <admin@jesuisundev.com>
2021-04-01 22:40:19 -0400

    moving starfield

--- public/libs/app_part_2.js
+++ public/libs/app_part_2.js
@@ -19,24 +19,33 @@ renderer.domElement.id = 'wormhole'
 
 document.body.appendChild(renderer.domElement)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.x = 0
 camera.position.y = 10
 camera.position.z = 0
 camera.lookAt(0, 0, 0)
 
+const starField = []
+const starTexture = new THREE.TextureLoader().load('../images/star.png')
+
+let starsMaterial, starsGeometry, stars
+let starCount = 50000
+let mouseX = 0,
+    mouseY = 0
+let windowHalfX = window.innerWidth / 2
+let windowHalfY = window.innerHeight / 2
+
+
 function getStarsGeometry() {
     const geometry = new THREE.BufferGeometry()
 
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(getStarsRandomVertices(), 3))
-
-    return geometry
+    return geometry.setAttribute('position', new THREE.Float32BufferAttribute(getVerticesInRandomPosition(), 3))
 }
 
-function getStarsRandomVertices(verticesNumber = 10000) {
+function getVerticesInRandomPosition() {
     const vertices = []
 
-    for (let i = 0; i < verticesNumber; i++) {
+    for (let i = 0; i < starCount; i++) {
         const x = 2000 * Math.random() - 1000;
         const y = 2000 * Math.random() - 1000;
         const z = 2000 * Math.random() - 1000;
@@ -48,19 +57,21 @@ function getStarsRandomVertices(verticesNumber = 10000) {
 }
 
 function getStarsMaterial() {
-    const starSprite = new THREE.TextureLoader().load('../images/star.png');
-    const starMaterial = new THREE.PointsMaterial({ size: 5, sizeAttenuation: true, map: starSprite, alphaTest: 0.5, transparent: true });
-
-    return starMaterial
+    return new THREE.PointsMaterial({
+        size: 5,
+        sizeAttenuation: true,
+        map: starTexture,
+        transparent: true
+    })
 }
 
-function getStars() {
-    const stars = new THREE.Points(getStarsGeometry(), getStarsMaterial())
+starsGeometry = getStarsGeometry()
+starsMaterial = getStarsMaterial()
 
-    return stars
-}
+stars = new THREE.Points(starsGeometry, starsMaterial)
+console.log(stars)
+scene.add(stars)
 
-scene.add(getStars())
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -68,10 +79,22 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix()
 })
 
+document.body.addEventListener('pointermove', onPointerMove);
+
+function onPointerMove(event) {
+    if (event.isPrimary === false) return
+
+    mouseX = event.clientX - windowHalfX
+    mouseY = event.clientY - windowHalfY
+}
+
 function animate(time) {
     if (needRender) {
-        renderer.render(scene, camera);
+        renderer.render(scene, camera)
     }
+
+    camera.position.x += (mouseX - camera.position.x) * 0.05
+    camera.position.y += (-mouseY - camera.position.y) * 0.05
 
     requestAnimationFrame(animate)
 }
